5b37cd664eeb3375cb09b156aee4af710647b2b6
joseph-aylor <joseph.aylor@gmail.com>
2016-09-10 23:48:30 -0400

    Added Fugitive, captoctrl for ubuntu desktop, and
    rotate.svg because keeping binary files in a git repo
    is cool.

--- .vim/bundle/repos.sh
+++ .vim/bundle/repos.sh
@@ -12,3 +12,4 @@ git clone https://github.com/airblade/vim-gitgutter.git;
 git clone https://github.com/vim-airline/vim-airline-themes.git;
 git clone https://github.com/leafgarland/typescript-vim.git;
 git clone https://github.com/HerringtonDarkholme/yats.vim.git
+git clone https://github.com/tpope/vim-fugitive.git
