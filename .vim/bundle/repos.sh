0a92f85a36376c0f13947822c9fc8dfac4e8021a
Joe A <jaylor@topicdesign.com>
2016-10-11 09:20:51 -0400

    bufkill

--- .vim/bundle/repos.sh
+++ .vim/bundle/repos.sh
@@ -13,5 +13,7 @@ git clone https://github.com/vim-airline/vim-airline-themes.git;
 git clone https://github.com/leafgarland/typescript-vim.git;
 git clone https://github.com/HerringtonDarkholme/yats.vim.git
 git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/ngmy/vim-rubocop.git
 git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/kchmck/vim-coffee-script.git
+git clone https://github.com/qpkorr/vim-bufkill
