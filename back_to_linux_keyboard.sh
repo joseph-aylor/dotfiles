cf96fef3eb0a69e3d614ab2f63857dba41fa3c51
Joseph Aylor <joseph.aylor@gmail.com>
2017-02-04 23:50:45 -0500

    got a mac keyboard

---
+++ back_to_linux_keyboard.sh
@@ -0,0 +1,4 @@
+xmodmap -e "keycode 133 = Super_L NoSymbol Super_L"
+xmodmap -e "keycode 64 = Alt_L Meta_L Alt_L Meta_L"
+xmodmap -e "keycode 134 = Super_L NoSymbol Super_L"
+xmodmap -e "keycode 108 = Alt_L Meta_L Alt_L Meta_L"
---
+++ normalize_mac_keyboard.sh
@@ -0,0 +1,4 @@
+xmodmap -e "keycode 64 = Super_L NoSymbol Super_L"
+xmodmap -e "keycode 133 = Alt_L Meta_L Alt_L Meta_L"
+xmodmap -e "keycode 108 = Super_L NoSymbol Super_L"
+xmodmap -e "keycode 134 = Alt_L Meta_L Alt_L Meta_L"
